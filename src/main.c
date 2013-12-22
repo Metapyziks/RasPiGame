550d04864373f6dc16944201017ff2825f2a8ae4
Metapyziks <metapyziks@gmail.com>
2013-12-22 23:52:31 +0000

    Flashy flashy

--- src/main.c
+++ src/main.c
@@ -6,17 +6,12 @@ int main(void)
 
     lcd_init();
 
-    for (y = 0; y < DISPLAY_HEIGHT; ++y) {
-        for (x = 0; x < DISPLAY_WIDTH; ++x) {
-            lcd_setPixel(x, y, clr_fromRGB(0x7f,
-                (x * 0xff) / DISPLAY_WIDTH,
-                (y * 0xff) / DISPLAY_HEIGHT));
-        }
+    for (int i = 0; i < 10; ++i) {
+        lcd_clear(CLR_WHITE);
+        sleep(1);
+        lcd_clear(CLR_BLACK);
+        sleep(1);
     }
-
-    sleep(5);
-
-    lcd_clear(CLR_BLACK);
 
     lcd_stop();
 
