e7d27d59e9de6bc24d56b16ecf6f848d5cf3859c
Metapyziks <metapyziks@gmail.com>
2013-12-23 00:55:14 +0000

    Pretty colours

--- src/main.c
+++ src/main.c
@@ -5,12 +5,24 @@ int main(void)
     lcd_init();
 
     for (int i = 0; i < 0x100; ++i) {
-        lcd_clear(clr_fromRGB(0x7f, 0xff - i, i));
-        for (volatile int j = 0; j < 1000000; ++j);
+        lcd_clear(clr_fromRGB(0x00, 0xff - i, i));
+        for (volatile int j = 0; j < 100000; ++j);
     }
-    
-    sleep(5);
-    lcd_clear(CLR_BLACK);
+
+    for (int i = 0; i < 0x100; ++i) {
+        lcd_clear(clr_fromRGB(i, 0x00, 0xff - i));
+        for (volatile int j = 0; j < 100000; ++j);
+    }
+
+    for (int i = 0; i < 0x100; ++i) {
+        lcd_clear(clr_fromRGB(0xff - i, i, 0x00));
+        for (volatile int j = 0; j < 100000; ++j);
+    }
+
+    for (int i = 0; i < 0x100; ++i) {
+        lcd_clear(clr_fromRGB(0x00, 0xff - i, 0x00));
+        for (volatile int j = 0; j < 100000; ++j);
+    }
 
     lcd_stop();
 
