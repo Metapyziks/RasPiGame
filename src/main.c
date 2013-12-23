bcd3a147326a0fca951f468ce0e63648301159ed
Metapyziks <metapyziks@gmail.com>
2013-12-23 00:51:54 +0000

    Delay for fade

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ int main(void)
 
     for (int i = 0; i < 0x100; ++i) {
         lcd_clear(clr_fromRGB(0x7f, 0xff - i, i));
+        for (volatile int j = 0; j < 10000; ++j);
     }
     
     sleep(5);
