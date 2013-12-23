709df14697da98523c8a890e7da32d76055460d6
Metapyziks <metapyziks@gmail.com>
2013-12-23 00:56:38 +0000

    Loop

--- src/main.c
+++ src/main.c
@@ -4,24 +4,28 @@ int main(void)
 {
     lcd_init();
 
-    for (int i = 0; i < 0x100; ++i) {
-        lcd_clear(clr_fromRGB(0x00, 0xff - i, i));
-        for (volatile int j = 0; j < 100000; ++j);
-    }
+    int delay = 200000;
 
-    for (int i = 0; i < 0x100; ++i) {
-        lcd_clear(clr_fromRGB(i, 0x00, 0xff - i));
-        for (volatile int j = 0; j < 100000; ++j);
-    }
+    for (int k = 0; k < 3; ++k) {
+        for (int i = 0; i < 0x100; ++i) {
+            lcd_clear(clr_fromRGB(0x00, 0xff - i, i));
+            for (volatile int j = 0; j < delay; ++j);
+        }
 
-    for (int i = 0; i < 0x100; ++i) {
-        lcd_clear(clr_fromRGB(0xff - i, i, 0x00));
-        for (volatile int j = 0; j < 100000; ++j);
+        for (int i = 0; i < 0x100; ++i) {
+            lcd_clear(clr_fromRGB(i, 0x00, 0xff - i));
+            for (volatile int j = 0; j < delay; ++j);
+        }
+
+        for (int i = 0; i < 0x100; ++i) {
+            lcd_clear(clr_fromRGB(0xff - i, i, 0x00));
+            for (volatile int j = 0; j < delay; ++j);
+        }
     }
 
     for (int i = 0; i < 0x100; ++i) {
         lcd_clear(clr_fromRGB(0x00, 0xff - i, 0x00));
-        for (volatile int j = 0; j < 100000; ++j);
+        for (volatile int j = 0; j < delay; ++j);
     }
 
     lcd_stop();
