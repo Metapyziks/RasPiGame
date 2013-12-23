1f3e253936b162bb3ab35ca5f4e5d23346abfcd9
Metapyziks <metapyziks@gmail.com>
2013-12-23 00:57:48 +0000

    Added fade-in

--- src/main.c
+++ src/main.c
@@ -6,6 +6,11 @@ int main(void)
 
     int delay = 200000;
 
+    for (int i = 0; i < 0x100; ++i) {
+        lcd_clear(clr_fromRGB(0x00, i, 0x00));
+        for (volatile int j = 0; j < delay; ++j);
+    }
+
     for (int k = 0; k < 3; ++k) {
         for (int i = 0; i < 0x100; ++i) {
             lcd_clear(clr_fromRGB(0x00, 0xff - i, i));
