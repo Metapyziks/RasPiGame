ea54d08f5c7e4d0f9eca5058bce76699aa2b0abd
Metapyziks <metapyziks@gmail.com>
2013-12-23 16:19:01 +0000

    Added ioctl number print

--- src/lcd.c
+++ src/lcd.c
@@ -1,7 +1,7 @@
 #include "lcd.h"
 
 #define SET_PIXEL(x, y, clr) (lcd_fbp[(x) + (y) * DISPLAY_WIDTH] = clr)
-#define SSD1289_GET_KEYS 0xe57d7301
+#define SSD1289_GET_KEYS _IOR('keys', 1, unsigned char *)
 
 static struct fb_fix_screeninfo lcd_finfo;
 static struct fb_var_screeninfo lcd_vinfo;
@@ -19,6 +19,8 @@ int lcd_init(void)
         perror("Error: cannot open framebuffer device");
         return FALSE;
     }
+    
+    printf("Ioctl Number: (int)%d  (hex)%x\n", SSD1289_GET_KEYS, SSD1289_GET_KEYS);
 
     printf("The framebuffer device was opened successfully.\n");
 
