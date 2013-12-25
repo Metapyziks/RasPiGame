30b19f89f6d79dd1bf7859314b5cfef9a168151a
Metapyziks <metapyziks@gmail.com>
2013-12-25 00:51:14 +0000

    Faster

--- src/main.c
+++ src/main.c
@@ -46,8 +46,6 @@ int main(void)
 
         lcd_blitTilesPalette(tileset, palette, 16, 16, tilesetW / 16,
             tiles, x, y, 160, 160, 80, 60, 160, 120);
-
-        usleep(16000);
     } while (!lcd_buttonDown(BTN_1 | BTN_4));
 
     lcd_clear(CLR_BLACK);
