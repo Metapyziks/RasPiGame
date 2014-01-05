3d6765cd3b3f8d72da2296b3e84f3043e9e097ab
Metapyziks <metapyziks@gmail.com>
2014-01-05 01:08:57 +0000

    Dungeon geometry generation almost finished

--- src/mapgen/dungeon.c
+++ src/mapgen/dungeon.c
@@ -3,12 +3,12 @@
 #include "../map.h"
 #include "../utils.h"
 
-#define SPLIT_HORZ 0
-#define SPLIT_VERT 1
-
 #define MIN_DIV 8
 
-static int nextVertID = 0;
+#define SPAN_MARK 1
+#define CARVE_MARK -1
+
+static int vertCount = 0;
 
 struct vert
 {
@@ -19,13 +19,19 @@ struct vert
     struct vert* r;
 };
 
+struct path
+{
+    struct vert* a;
+    struct vert* b;
+};
+
 static struct vert* vert_new(int x, int y)
 {
     struct vert* v = NEW(struct vert, 1);
 
     v->x = x;
     v->y = y;
-    v->id = nextVertID++;
+    v->id = vertCount++;
     v->mark = 0;
 
     v->t = NULL;
@@ -172,9 +178,157 @@ static void subdivide(struct vert* tl)
     }
 }
 
-static void cullPaths(struct map map, struct vert* v)
+static void fillVertsArray(struct vert** verts, struct vert* v)
 {
+    verts[v->id] = v;
 
+    if (v->t != NULL && verts[v->t->id] == NULL) fillVertsArray(verts, v->t);
+    if (v->l != NULL && verts[v->l->id] == NULL) fillVertsArray(verts, v->l);
+    if (v->b != NULL && verts[v->b->id] == NULL) fillVertsArray(verts, v->b);
+    if (v->r != NULL && verts[v->r->id] == NULL) fillVertsArray(verts, v->r);
+}
+
+static struct vert** findVerts(struct vert* v)
+{
+    struct vert** verts = NEW(struct vert*, vertCount);
+
+    for (int i = 0; i < vertCount; ++ i) verts[i] = NULL;
+
+    fillVertsArray(verts, v);
+
+    return verts;
+}
+
+/*
+static int countPaths(struct vert** verts)
+{
+    int sum = 0;
+    for (int i = 0; i < vertCount; ++i) {
+        if (verts[i]->t != NULL) ++sum;
+        if (verts[i]->l != NULL) ++sum;
+        if (verts[i]->b != NULL) ++sum;
+        if (verts[i]->r != NULL) ++sum;
+    }
+
+    return sum / 2;
+}
+*/
+
+static void fillPathsArray(struct path* paths, struct vert* v);
+
+static void addPathToArray(struct path* paths, struct vert* a, struct vert* b)
+{
+    struct vert* next = b;
+
+    if (b->id < a->id) {
+        b = a; a = next;
+    }
+
+    int i;
+    for (i = 0; i < vertCount * 4 && paths[i].a != NULL; ++i) {
+        if (paths[i].a == a && paths[i].b == b) return;
+    }
+
+    paths[i].a = a; paths[i].b = b;
+    
+    fillPathsArray(paths, next);
+}
+
+static void fillPathsArray(struct path* paths, struct vert* v)
+{
+    if (v->t != NULL) addPathToArray(paths, v, v->t);
+    if (v->l != NULL) addPathToArray(paths, v, v->l);
+    if (v->b != NULL) addPathToArray(paths, v, v->b);
+    if (v->r != NULL) addPathToArray(paths, v, v->r);
+}
+
+static struct path* findPaths(struct vert** verts)
+{
+    struct path* paths = NEW(struct path, vertCount * 4);
+
+    for (int i = 0; i < vertCount * 4; ++ i) {
+        paths[i].a = NULL; paths[i].b = NULL;
+    }
+
+    fillPathsArray(paths, verts[0]);
+
+    return paths;
+}
+
+static int countPaths(struct path* paths) {
+    int count;
+    for (count = 0; count < vertCount * 4 && paths[count].a != NULL; ++count);
+    return count;
+}
+
+static void removePath(struct path* paths, struct path path)
+{
+    if (path.a->t == path.b) { path.a->t = NULL; path.b->b = NULL; } else 
+    if (path.a->l == path.b) { path.a->l = NULL; path.b->r = NULL; } else 
+    if (path.a->b == path.b) { path.a->b = NULL; path.b->t = NULL; } else 
+    if (path.a->r == path.b) { path.a->r = NULL; path.b->l = NULL; }
+
+    int found = FALSE;
+    for (int i = 0; i < vertCount * 4; ++i) {
+        if (!found && paths[i].a == path.a && paths[i].b == path.b) found = TRUE;
+        if (found) paths[i] = paths[i + 1];
+        if (paths[i].a == NULL) break;
+    }
+}
+
+static void restorePath(struct path path)
+{
+    if (path.a->y > path.b->y) { path.a->t = path.b; path.b->b = path.a; } else
+    if (path.a->x > path.b->x) { path.a->l = path.b; path.b->r = path.a; } else
+    if (path.a->y < path.b->y) { path.a->b = path.b; path.b->t = path.a; } else
+    if (path.a->x < path.b->x) { path.a->r = path.b; path.b->l = path.a; }
+}
+
+static void markNetwork(struct vert** verts, int value)
+{
+    for (int i = 0; i < vertCount; ++i) verts[i]->mark = value;
+}
+
+static void floodNetwork(struct vert* v, int value)
+{
+    v->mark = value;
+
+    if (v->t != NULL && v->t->mark != value) floodNetwork(v->t, value);
+    if (v->l != NULL && v->l->mark != value) floodNetwork(v->l, value);
+    if (v->b != NULL && v->b->mark != value) floodNetwork(v->b, value);
+    if (v->r != NULL && v->r->mark != value) floodNetwork(v->r, value);
+}
+
+static void cullPaths(struct vert* v)
+{
+    struct vert** verts = findVerts(v);
+    struct path* paths = findPaths(verts);
+
+    int pathCount = countPaths(paths);
+    int currCount = pathCount;
+    int destCount = vertCount;
+
+    markNetwork(verts, 0);
+
+    while (pathCount > destCount && currCount > 0) {
+        int index = rand() % currCount--;
+        struct path path = paths[index];
+
+        removePath(paths, path);
+        floodNetwork(path.a, 1 - path.a->mark);
+
+        if (path.b->mark != path.a->mark) {
+            floodNetwork(path.b, path.a->mark);
+            restorePath(path);
+        } else {
+            pathCount--;
+        }
+    }
+
+    markNetwork(verts, 0);
+
+    free(verts);
+    free(paths);
 }
 
 static void carvePath(struct map map, struct vert* a, struct vert* b,
@@ -193,43 +347,41 @@ static void carvePath(struct map map, struct vert* a, struct vert* b,
 static void carveNetwork(struct map map, struct vert* v,
     void (*hollowFunc)(struct map, int, int, int, int))
 {
-    v->mark = -1;
+    v->mark = CARVE_MARK;
 
-    if (v->t != NULL && v->t->mark != -1) carvePath(map, v, v->t, hollowFunc);
-    if (v->l != NULL && v->l->mark != -1) carvePath(map, v, v->l, hollowFunc);
-    if (v->b != NULL && v->b->mark != -1) carvePath(map, v, v->b, hollowFunc);
-    if (v->r != NULL && v->r->mark != -1) carvePath(map, v, v->r, hollowFunc);
+    if (v->t != NULL && v->t->mark != CARVE_MARK) carvePath(map, v, v->t, hollowFunc);
+    if (v->l != NULL && v->l->mark != CARVE_MARK) carvePath(map, v, v->l, hollowFunc);
+    if (v->b != NULL && v->b->mark != CARVE_MARK) carvePath(map, v, v->b, hollowFunc);
+    if (v->r != NULL && v->r->mark != CARVE_MARK) carvePath(map, v, v->r, hollowFunc);
 
-    if (v->t != NULL && v->t->mark != -1) carveNetwork(map, v->t, hollowFunc);
-    if (v->l != NULL && v->l->mark != -1) carveNetwork(map, v->l, hollowFunc);
-    if (v->b != NULL && v->b->mark != -1) carveNetwork(map, v->b, hollowFunc);
-    if (v->r != NULL && v->r->mark != -1) carveNetwork(map, v->r, hollowFunc);
+    if (v->t != NULL && v->t->mark != CARVE_MARK) carveNetwork(map, v->t, hollowFunc);
+    if (v->l != NULL && v->l->mark != CARVE_MARK) carveNetwork(map, v->l, hollowFunc);
+    if (v->b != NULL && v->b->mark != CARVE_MARK) carveNetwork(map, v->b, hollowFunc);
+    if (v->r != NULL && v->r->mark != CARVE_MARK) carveNetwork(map, v->r, hollowFunc);
 }
 
 static void freeVerts(struct vert* v)
 {
-    if (v->t != NULL) v->t->b = NULL;
-    if (v->l != NULL) v->l->r = NULL;
-    if (v->b != NULL) v->b->t = NULL;
-    if (v->r != NULL) v->r->l = NULL;
+    struct vert** verts = findVerts(v);
 
-    if (v->t != NULL) freeVerts(v->t);
-    if (v->l != NULL) freeVerts(v->l);
-    if (v->b != NULL) freeVerts(v->b);
-    if (v->r != NULL) freeVerts(v->r);
+    for (int i = 0; i < vertCount; ++i) free(verts[i]);
 
-    free(v);
+    free(verts);
+
+    vertCount = 0;
 }
 
 void map_genDungeon(struct map map, int x, int y, int w, int h,
     void (*hollowFunc)(struct map, int, int, int, int),
     void (*solidFunc)(struct map, int, int, int, int))
 {
+
     struct vert* tl = quad(x + 8, y + 8, w - 16, h - 16);
 
     subdivide(tl);
+    cullPaths(tl);
     carveNetwork(map, tl, hollowFunc);
-    solidFunc(map, x + 4, y + 4, w - 8, h - 8);
+    solidFunc(map, x, y, w, h);
 
-    // freeVerts(tl);
+    freeVerts(tl);
 }
